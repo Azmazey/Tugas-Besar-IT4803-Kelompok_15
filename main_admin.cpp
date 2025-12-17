32715983863555786c629be43a600d87b4fd9e81
Azmazey <azmasyabila@gmail.com>
2025-12-17 20:58:40 +0700

    Update main_admin.cpp

--- main_admin.cpp
+++ main_admin.cpp
@@ -1,85 +1,81 @@
 #include "main.h"
+#include <iostream>
+using namespace std;
 
-void menuAdmin(){
-    int option=-99;
-    while (option != 0) {
+void menuAdmin(listBis &LB, listPenumpang &LP, listRelasi &LR){
+    int opt;
+    do{
         system("cls");
-        cout << "============ Menu Admin============ " << endl;
-        cout << "|| 1. Parent                 ||" << endl;
-        cout << "|| 2. Child                  ||" << endl;
-        cout << "|| 0. back                   ||" << endl;
-        cout << "============================== " << endl;
-        cout << "Choose your option : ";
-        cin >> option;
-        switch(option) {
-           case 1  :
-              cout << "you choose option 1" << endl;
-              menuParent();
-              continue;
-           case 2  :
-              cout << "you choose option 2" << endl;
-              menuChild();
-              continue;
+        cout << "MENU ADMIN\n";
+        cout << "1. Kelola Bis\n";
+        cout << "2. Kelola Penumpang\n";
+        cout << "0. Back\n";
+        cin >> opt;
+
+        if(opt == 1){
+            menuParent(LB);
         }
-    }
+        else if(opt == 2){
+            menuChild(LP);
+        }
+    }while(opt != 0);
 }
 
+void menuParent(listBis &LB){
+    int opt;
+    Bis x;
+    adrBis P;
 
-void menuParent(){
-    int option=-99;
-    while (option != 0) {
+    do{
         system("cls");
-        cout << "============ Menu Parent============ " << endl;
-        cout << "|| 1. insert first          ||" << endl;
-        cout << "|| 2. insert last           ||" << endl;
-        cout << "|| .....dst                 ||" << endl;
-        cout << "|| 0. back                  ||" << endl;
-        cout << "============================== " << endl;
-        cout << "Choose your option : ";
-        cin >> option;
-        switch(option) {
-           case 1  :
-              cout << "you choose option 1" << endl;
-              // write your code here
+        cout << "MENU BIS\n";
+        cout << "1. Insert Bis\n";
+        cout << "2. Show Bis\n";
+        cout << "0. Back\n";
+        cin >> opt;
 
-              insertfirstparent(L,P)
-              break;
-           case 2  :
-              cout << "you choose option 2" << endl;
-              // write your code here
+        if(opt == 1){
+            cin.ignore();
+            cout << "Nama Bis : "; getline(cin, x.namaBis);
+            cout << "Rute     : "; getline(cin, x.rute);
+            cout << "Kapasitas: "; cin >> x.kapasitas;
 
-              break;
-
-            // continue for another case here
-
+            P = createElement(x);
+            insertLast(LB, P);
         }
-    }
+        else if(opt == 2){
+            showAllBis(LB);
+            system("pause");
+        }
+
+    }while(opt != 0);
 }
 
-void menuChild(){
-    int option=-99;
-    while (option != 0) {
+void menuChild(listPenumpang &LP){
+    int opt;
+    infotypePenumpang x;
+    adrPenumpang P;
+
+    do{
         system("cls");
-        cout << "============ Menu Child============ " << endl;
-        cout << "|| 1. insert first          ||" << endl;
-        cout << "|| 2. insert last           ||" << endl;
-        cout << "|| .....dst                 ||" << endl;
-        cout << "|| 0. back                  ||" << endl;
-        cout << "============================== " << endl;
-        cout << "Choose your option : ";
-        cin >> option;
-        switch(option) {
-           case 1  :
-              cout << "you choose option 1" << endl;
-              // write your code here
+        cout << "MENU PENUMPANG\n";
+        cout << "1. Insert Penumpang\n";
+        cout << "2. Show Penumpang\n";
+        cout << "0. Back\n";
+        cin >> opt;
 
-              break;
-           case 2  :
-              cout << "you choose option 2" << endl;
-              // write your code here
+        if(opt == 1){
+            cin.ignore();
+            cout << "Nama   : "; getline(cin, x.nama);
+            cout << "Tujuan : "; getline(cin, x.tujuan);
 
-              break;
-            // continue for another case here
+            P = createElmPenumpang(x);
+            insertLastPenumpang(LP, P);
         }
-    }
+        else if(opt == 2){
+            showPenumpang(LP);
+            system("pause");
+        }
+
+    }while(opt != 0);
 }
