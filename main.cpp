2f07e6f800dcb8b30c7392527849198c71aedaaa
dinalusiyana <dinalusiyana09@gmail.com>
2025-12-17 22:12:09 +0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include "bis.h"
+#include "penumpang.h"
 #include "main.h"
 
 using namespace std;
@@ -6,28 +8,36 @@ using namespace std;
 int main(){
     listBis LB;
     listPenumpang LP;
-    listRelasi LR;
 
     createList(LB);
     createListPenumpang(LP);
-    createListRelasi(LR);
 
-    int opt;
-    do{
+    int option = -99;
+
+    while (option != 0) {
         system("cls");
-        cout << "1. Admin\n";
-        cout << "2. User\n";
-        cout << "0. Exit\n";
-        cin >> opt;
+        cout << "============ Menu ============ " << endl;
+        cout << "|| 1. Admin                 ||" << endl;
+        cout << "|| 2. User                  ||" << endl;
+        cout << "|| 0. Exit                  ||" << endl;
+        cout << "============================== " << endl;
+        cout << "Choose your option : ";
+        cin >> option;
 
-        if(opt == 1){
-            menuAdmin(LB, LP, LR);
+        if (option == 1) {
+            menuAdmin(LB, LP);
         }
-        else if(opt == 2){
-            menuUser(LB, LP, LR);
+        else if (option == 2) {
+            menuUser(LB, LP);
         }
-
-    }while(opt != 0);
+        else if (option == 0) {
+            cout << "Program selesai." << endl;
+        }
+        else {
+            cout << "Pilihan tidak tersedia." << endl;
+            system("pause");
+        }
+    }
 
     return 0;
 }
